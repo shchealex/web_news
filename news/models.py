526bd1023a081f52df39e9125d7ddccc5e81d2da
Щербаков_АВ <shchealex@icloud.com>
2022-10-25 16:15:11 +0500

    Задание 2.7.1

--- news/models.py
+++ news/models.py
@@ -1,27 +1,20 @@
 from django.db import models
 from datetime import datetime
-
-director = 'DI'
-admin = 'AD'
-cook = 'CO'
-cashier = 'CA'
-cleaner = 'CL'
-
-POSITIONS = [
-    (director, 'Директор'),
-    (admin, 'Администратор'),
-    (cook, 'Повар'),
-    (cashier, 'Кассир'),
-    (cleaner, 'Уборщик')
-]
+from news.resources import POSITIONS, cashier
 
 
 class Staff(models.Model):
+    director = 'DI'
+    admin = 'AD'
+    cook = 'CO'
+    cashier = 'CA'
+    cleaner = 'CL'
+
     full_name = models.CharField(max_length=255)
     position = models.CharField(max_length=2,
                                 choices=POSITIONS,
                                 default=cashier)
-    labor_contact = models.IntegerField()
+    labor_contract = models.IntegerField()
     def get_last_name(self):
         return self.full_name.split()[0]
 
@@ -35,10 +28,9 @@ class Order(models.Model):
     time_in = models.DateTimeField(auto_now_add=True)
     time_out = models.DateTimeField(null=True)
     cost = models.FloatField(default=0.0)
-    pickup = models.BooleanField(default=False)
+    take_away = models.BooleanField(default=False)
     complete = models.BooleanField(default=False)
     staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
-
     products = models.ManyToManyField(Product, through='ProductOrder')
 
     def finish_order(self):
@@ -57,8 +49,17 @@ class Order(models.Model):
 class ProductOrder(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    amount = models.IntegerField(default=1)
+    _amount = models.IntegerField(default=1, db_column = 'amount')
 
     def product_sum(self):
         product_price = self.product.price
         return product_price * self.amount
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @amount.setter
+    def amount(self, value):
+        self._amount = int(value) if value >= 0 else 0
+        self.save()
